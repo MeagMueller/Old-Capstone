f2498debf0642844af1416748f3cc74d5196b1b6
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-04-30 11:21:49 -0500

    Adding notes on Login, will be doing more research and adding more notes, worked on database

---
+++ src/scripts/notes.js
@@ -0,0 +1,14 @@
+// Given a user wants access Taksimplicity
+// When a opens the application
+// Then the user should see a welcome message
+    // landing page? Welcome message in pop up or just on page? **definitely click button to get to form** 
+// And see a form to input a username and password
+    // after button click, populates on page
+// And click an affordance to process the information
+    // waiting screen? Probably not necessary
+
+// Given a user enters their information
+// When the user clicks the affordance
+// Then the user should be taken to their personal account information
+    // session storage and what not -- this is super neat
+
