8d937d9fc30c55ff94d60632f4460d5b9c5ffd0f
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-04-30 11:50:39 -0500

    Adding more notes

--- src/scripts/notes.js
+++ src/scripts/notes.js
@@ -1,3 +1,5 @@
+// *****ISSUE 1: LOGIN*****
+
 // Given a user wants access Taksimplicity
 // When a opens the application
 // Then the user should see a welcome message
@@ -11,4 +13,59 @@
 // When the user clicks the affordance
 // Then the user should be taken to their personal account information
     // session storage and what not -- this is super neat
+
+// *****ISSUE 2: ADD PERFORMANCE*****
+
+// Given a user wants to enter a new performance/workshop/etc.
+// When the user clicks an affordance
+    // button 
+// Then the user should be presented with a form with the following properties
+    // inputs with the following
+// Name of event
+// Location
+// Date
+// And have a checkbox to click if the performance is paid or not
+    // checkbox availability
+// And have the ability to specify whether this is a solo performance or a troupe
+    // another checkbox, and then maybe a drop down to choose which troupe if in multiple?
+// And have the ability to select a dance style for the performance
+    // drop down, most likely - names in alphabetical order
+// And have the ability to add props if necessary
+    // another drop down
+    
+// Given a user specifies that a performance is with a troupe
+// When a user is presented with his or her events
+// Then troupe and personal should be differentiated
+    // troupe performances in different colors, colors chosen when adding a troupe to your account - might be a stretch goal!
+
+// *****ISSUE 3: EDIT PERFORMANCE*****
+
+// Given a user wants to edit a performance/workshop/etc.
+// When the user clicks an “edit” affordance
+    // button
+// Then the user should be presented with the information in a prefilled-form
+    // pre-filled inputs -research this soon
+
+// Given the user has completed editing the above event
+// When the user clicks on a save affordance or presses enter
+    // save button and enter press - I believe was available back several projects ago - enter is a specific number with a keypress event
+// Then the information should be updated and the display should reflect the change
+    // PATCH and GET and POST
+
+// Given a user wants to remove a performance/workshop/etc.
+// When a user clicks a “remove” affordance
+    // button
+// Then the user should be presented with a confirmation pop up
+    // alert, perhaps? With buttons? Does that happen?
+// And should be able to confirm or deny the deletion
+
+// Given a user confirms a delete
+    // clicks the button, Frank
+// When the user is returned to the events screen
+    // not sure how to do this rerouting business
+// Then the information should be removed and the display should reflect the change
+    // GET and POST here
+
+
+
 
