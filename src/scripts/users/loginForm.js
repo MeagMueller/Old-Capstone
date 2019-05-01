10dcba6650d2286629eb9c4fac0bcabc60e8c3a4
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-05-01 11:48:10 -0500

    Still working on login

--- src/scripts/users/loginForm.js
+++ src/scripts/users/loginForm.js
@@ -4,12 +4,37 @@ const loginForm = {
     createLogin() {
         let loginFragment = document.createDocumentFragment()
 
-        let loginDiv = createHTML.createElement("div", "", "loginDiv")
-        // loginDiv.classlist.add("")
-        let welcomeHeader = loginDiv.appendChild(createHTML.createElement("h1", "Taksimplicity"))
+        let mainLoginDiv = createHTML.createElement("div", "", "mainLoginDiv")
+        // mainLoginDiv.classlist.add("")
+        let welcomeHeader = mainLoginDiv.appendChild(createHTML.createElement("h1", "Taksimplicity"))
         // welcomeHeader.classlist.add("")
-        let login = loginDiv.appendChild(createHTML.createElement("div"))
+        let loginContainer = mainLoginDiv.appendChild(createHTML.createElement("div"))
         // login.classlist.add("")
+        let loginFormElement = loginContainer.appendChild(createHTML.createElement("form"))
+        let loginformDiv = loginFormElement.appendChild(createHTML.createElement("div"))
+        // loginDiv.classlist.add("")
+
+        let usernameLabel = createHTML.createElement("label", "Username: ", "usernameLabel")
+        let usernameInput = createHTML.createElement("input", "", "usernameInput")
+        usernameInput.placeholder = "Username"
+
+        let emailDiv = loginFormElement.appendChild(createHTML.createElement("div", "", ""))
+        // emailDiv.classlist.add("")
+        let emailLabel = createHTML.createElement("label", "Email: ", "")
+        let emailInput = createHTML.createElement("input", "", "emailInput")
+        emailInput.placeholder = "Email"
+
+        let loginButtonDiv = loginFormElement.appendChild(createHTML.createElement("div"))
+        let loginButton = loginButtonDiv.appendChild(createHTML.createElement("button", "Login", "loginButton"))
+        loginButton.type = "button"
+
+        loginButton.addEventListener("click", loginHandler.login)
+
+        loginformDiv.appendChild(usernameLabel)
+        loginformDiv.appendChild(usernameInput)
+        emailDiv.appendChild(emailLabel)
+        emailDiv.appendChild(emailInput)
         
+
     }
 }
